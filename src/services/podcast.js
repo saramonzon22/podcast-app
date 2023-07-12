efcb9533e7a6c0db3d488d79dfae7aa6d2ea6b8e
Sara Monzón Quesada <sara.mon.quesada@gmail.com>
2023-07-12 13:03:46 +0100

    feat(podcast-app): update podcast service to store data in both cases

--- src/services/podcast.js
+++ src/services/podcast.js
@@ -1,18 +1,26 @@
+function checkLocalStorage(keyName, maxHoursValue) {
+    const storedData = localStorage.getItem(keyName);
+    const storedTimeLS = localStorage.getItem(`${keyName}Time`);
+
+    if (storedData && storedTimeLS) {
+        const currentTime = new Date().getTime();
+        const storedTime = new Date(storedTimeLS).getTime();
+        const differenceHours = (currentTime - storedTime) / (1000 * 60 * 60);
+
+        if (differenceHours < maxHoursValue) {
+            return JSON.parse(storedData);
+        }
+    }
+
+    return null;
+}
+
 async function getTop100() {
     try {
-        const storedData = localStorage.getItem('top100Podcasts');
-        const storedTimeLS = localStorage.getItem('top100PodcastsTimestamp');
-
-        if (storedData && storedTimeLS) {
-            const currentTime = new Date().getTime();
-            const storedTime = new Date(storedTimeLS).getTime();
-            const differenceHours = (currentTime - storedTime) / (1000 * 60 * 60);
-
-            if (differenceHours < 24) {
-                return JSON.parse(storedData);
-            }
+        const cachedData = checkLocalStorage('top100Podcasts', 24);
+        if (cachedData) {
+            return cachedData;
         }
-
         const response = await fetch(
             'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json'
         );
@@ -22,7 +30,7 @@ async function getTop100() {
         const formattedResponse = await response.json();
 
         localStorage.setItem('top100Podcasts', JSON.stringify(formattedResponse.feed.entry));
-        localStorage.setItem('top100PodcastsTimestamp', new Date().toISOString());
+        localStorage.setItem('top100PodcastsTime', new Date().toISOString());
 
         return formattedResponse.feed.entry;
     } catch (error) {
@@ -32,6 +40,10 @@ async function getTop100() {
 
 async function getSinglePodcast(podcastId) {
     try {
+        const cachedData = checkLocalStorage(`podcast-${podcastId}`, 24);
+        if (cachedData) {
+            return cachedData;
+        }
         const response = await fetch(`https://itunes.apple.com/lookup?id=${podcastId}&media=podcast&entity=podcastEpisode&limit=100`);
 
         if (!response.ok) {
@@ -39,6 +51,8 @@ async function getSinglePodcast(podcastId) {
         }
 
         const formattedSinglePodcast = await response.json();
+        localStorage.setItem(`podcast-${podcastId}`, JSON.stringify(formattedSinglePodcast.results));
+        localStorage.setItem(`podcast-${podcastId}Time`, new Date().toISOString());
         return formattedSinglePodcast.results;
     } catch (error) {
         console.error('Ocurrió un error:', error);
